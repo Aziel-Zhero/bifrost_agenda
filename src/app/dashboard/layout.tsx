bb3de7da6016eecbbb52f181fc5d32a6d9460a0e
Eduarda Cristina <eduarda.blue03@gmail.com>
2025-09-10 19:52:16 +0000

    4:50:56 PM: Netlify Build

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import type { ReactNode } from "react";
 import Header from "@/components/dashboard/header";
 
+export const dynamic = 'force-dynamic';
+
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen w-full flex-col">
