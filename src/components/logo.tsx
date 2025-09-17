186d041eb8e374ecc4c682bd594f88f07b2ed6a4
Eduarda Cristina <eduarda.blue03@gmail.com>
2025-09-17 21:00:57 +0000

    use esse icone mantendo a cor dele em todo o sistema subistituindo pela

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,11 +11,10 @@ export function Logo({ isHeader = false, title }: LogoProps) {
   return (
     <div className="flex items-center gap-2" aria-label="Agenda Logo">
       <Image 
-        src="/logo_calendario.png"
+        src="/logo_agenda.png"
         alt="Agenda Logo"
         width={isHeader ? 28 : 40}
         height={isHeader ? 28 : 40}
-        className={cn(isHeader ? "brightness-0 invert" : "")}
         priority
       />
       {title && <span className={cn("font-semibold", isHeader ? "text-lg text-white" : "text-2xl")}>{title}</span>}
