40291c63859343b8613ab0bf9a1ac0b670f7e93d
Eduarda Cristina <eduarda.blue03@gmail.com>
2025-09-17 18:33:04 +0000

    Adicione isso ao seu @layer components: @layer components {   .menu-glas

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -140,7 +140,7 @@ export default function Header() {
           </SheetTrigger>
           <SheetContent 
             side="left" 
-            className="menu-glass-effect p-0 text-white"
+            className="menu-glass p-0 text-white border-none"
           >
              <SheetHeader className="p-6">
                 <SheetTitle className="sr-only">Menu Principal</SheetTitle>
